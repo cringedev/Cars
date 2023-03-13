cb4332713bf47fb8ea5973cbb40b256b75ae7c02
vlad.kiykouski <vkiykouski@gmail.com>
2023-03-13 13:04:29 +0300

    init

---
+++ Cars.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Cars.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.Api.Controllers;
+
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost("token")]
+    public IActionResult GetToken([FromBody] UserCredentials credentials)
+    {
+        // Проверить логин и пароль пользователя
+        if (credentials.Username == "admin" && credentials.Password == "admin123")
+        {
+            // Создать токен
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("secret_key_for_token_generation");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, credentials.Username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            // Вернуть токен
+            return Ok(new { Token = tokenString });
+        }
+
+        // Вернуть ошибку в случае неверных логина или пароля
+        return Unauthorized();
+    }
+}
---
+++ Cars.Api/Models/UserCredentials.cs
@@ -0,0 +1,7 @@
+﻿namespace Cars.Api.Models;
+
+public class UserCredentials
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
